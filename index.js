5950eb68eaa53e42c01eb3e17fbbf035814de94a
Lachlan Phillips <flipsidelachy@gmail.com>
2019-03-30 22:35:04 +1030

    Reformat; split into providers; ESV provider half-rewritten

--- index.js
+++ index.js
@@ -1,179 +1,72 @@
-const childProcess = require('child_process')
 const fs = require('fs')
 const path = require('path')
 const { promisify } = require('util')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
 
+const esvProvider = require('./src/esv_provider.js')
 
 const dataDirectory = path.join(__dirname, 'data')
+const configFile = path.join(dataDirectory, 'config.json')
 const historyFile = path.join(dataDirectory, 'history.json')
-const audioCacheDirectory = path.join(dataDirectory, 'audio')
 const defaultUsername = 'lachlan'
-const apiKey = process.env.API_KEY
-const sweepAgeDays = process.env.SWEEP_AGE_DAYS || 2
 
-const estimatedAnnualReadingDays = Math.floor(365.25 / 7 * 5)
-
-
-const bookChapterCounts = {
-  "Genesis": 50,
-  "Exodus": 40,
-  "Leviticus": 27,
-  "Numbers": 36,
-  "Deuteronomy": 34,
-  "Joshua": 24,
-  "Judges": 21,
-  "Ruth": 4,
-  "1 Samuel": 31,
-  "2 Samuel": 24,
-  "1 Kings": 22,
-  "2 Kings": 25,
-  "1 Chronicles": 29,
-  "2 Chronicles": 36,
-  "Ezra": 10,
-  "Nehemiah": 13,
-  "Esther": 10,
-  "Job": 42,
-  "Psalms": 150,
-  "Proverbs": 31,
-  "Ecclesiastes": 12,
-  "Song of Solomon": 8,
-  "Isaiah": 66,
-  "Jeremiah": 52,
-  "Lamentations": 5,
-  "Ezekiel": 48,
-  "Daniel": 12,
-  "Hosea": 14,
-  "Joel": 3,
-  "Amos": 9,
-  "Obadiah": 1,
-  "Jonah": 4,
-  "Micah": 7,
-  "Nahum": 3,
-  "Habakkuk": 3,
-  "Zephaniah": 3,
-  "Haggai": 2,
-  "Zechariah": 14,
-  "Malachi": 4,
-  "Matthew": 28,
-  "Mark": 16,
-  "Luke": 24,
-  "John": 21,
-  "Acts": 28,
-  "Romans": 16,
-  "1 Corinthians": 16,
-  "2 Corinthians": 13,
-  "Galatians": 6,
-  "Ephesians": 6,
-  "Philippians": 4,
-  "Colossians": 4,
-  "1 Thessalonians": 5,
-  "2 Thessalonians": 3,
-  "1 Timothy": 6,
-  "2 Timothy": 4,
-  "Titus": 3,
-  "Philemon": 1,
-  "Hebrews": 13,
-  "James": 5,
-  "1 Peter": 5,
-  "2 Peter": 3,
-  "1 John": 5,
-  "2 John": 1,
-  "3 John": 1,
-  "Jude": 1,
-  "Revelation": 22
-}
-
-const rawLists = {
-  "NT history":
-    ["Matthew", "Mark", "Luke", "John", "Acts", "Revelation"],
-  "Psalms":
-    ["Psalms"],
-  "OT history":
-    ["Genesis", "Job", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther"],
-  "Proverbs":
-    ["Proverbs"],
-  "OT prophets":
-    ["Ecclesiastes", "Song of Solomon", "Lamentations", "Isaiah", "Jeremiah", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi"],
-  "NT letters":
-    ["Romans", "Hebrews", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "1 Timothy", "2 Timothy", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "Titus", "Philemon", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude"],
-}
-
-
-//
-// core data structures
-//
-
-const lists = Object.keys(rawLists).map(name => {
-  const books = rawLists[name]
-  const chapterSequence = [].concat(...books.map(b => {
-    const bookLength = bookChapterCounts[b]
-    if (bookLength === 1) return [b]
-    else return rangeUntil(bookLength).map(i => b + ' ' + (i + 1))
-  }))
-  return {
-    name,
-    books,
-    chapterCount: chapterSequence.length,
-    chapterSequence,
-    yearFrequency: toD1(estimatedAnnualReadingDays / chapterSequence.length)
+const providers = {
+  esv: esvProvider,
+  file: {
+    name: 'file',
+    getNext () { return 'null' },
+    getAudio () { return Promise.resolve('') }
   }
-})
-
-const state = lists.map(l => ({ name: l.name, index: -1, playSequenceClock: null }))
-
+}
 
 //
 // startup logic
 //
 
-loadHistory(defaultUsername)
-  .then(entries => {
-    entries.forEach(e => updatePlaceToChapter(e.listName, e.chapter))
-
-    lists.forEach((l, idx) => {
-      console.log({
-        l: l.chapterCount,
-        n: l.name,
-        next: getNextChapterByPlace(l.name)
-      })
+loadConfig()
+  .then(config => {
+    Object.keys(providers).map(id => {
+      providers[id].config = config.providers[id] || {}
     })
-    console.log(getPlaylist())
 
-    startServer()
+    loadHistory(defaultUsername)
+      .then(entries => {
+        entries.reverse()
+
+        const initialState = {}
+        config.lists.forEach(list => {
+          const newest = entries.find(e => e.listName === list.name) || null
+          initialState[list.name] = newest ? newest.reference : null
+        })
+
+        console.log(getPlaylist(config, initialState))
+        startServer(config, initialState)
+      })
   })
-
 
 //
 // web server
 //
 
-function startServer () {
-  if (!apiKey) {
-    console.log('API_KEY must be set')
-    return
-  }
-
-
+function startServer (config, initialState) {
   const app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
 
   app.get('/', (req, res) => {
-    const playlist = getPlaylist()
+    const playlist = getPlaylist(config, initialState)
 
     const entriesJson = JSON.stringify(playlist)
     const agendaHtml = playlist
       .map((entry, idx) => {
         return `
-        <div id='entry-${idx}'>${entry.list} - ${entry.chapter}</div>`
+        <div id='entry-${idx}'>${entry.name} - ${entry.reference}</div>`
       })
       .join('\n')
 
-    preloadAudios(playlist.map(p => p.chapter))
+    preloadAudios(playlist)
 
     res.send(`
 <!doctype html>
@@ -201,8 +94,8 @@ function startServer () {
       }
 
       function applySrc (entry) {
-        currentlyPlayingEl.innerText = entry.list + ' - ' + entry.chapter
-        mediaElement.src = 'chapter-audio?q=' + encodeURIComponent(entry.chapter) + '&x=' + Math.random()
+        currentlyPlayingEl.innerText = entry.name + ' - ' + entry.reference
+        mediaElement.src = 'audio?p=' + entry.provider + '&q=' + encodeURIComponent(entry.reference) + '&x=' + Math.random()
         mediaElement.load(); // important on iOS
 
         try {
@@ -231,9 +124,9 @@ function startServer () {
       }
 
       function endedListener (evt) {
-        // TODO: report finished chapter
+        // TODO: report finished audio
         var entry = entries[currentItem]
-        fetch('record-history?list=' + encodeURIComponent(entry.list) + '&chapter=' + encodeURIComponent(entry.chapter), { method: 'POST' })
+        fetch('record-history?list=' + encodeURIComponent(entry.name) + '&reference=' + encodeURIComponent(entry.reference), { method: 'POST' })
           .then(() => console.log('recorded'), e => console.warn('failed to record', e))
 
         currentItem++;
@@ -248,23 +141,23 @@ function startServer () {
 </html>`)
   })
 
-  app.get('/chapter-audio', (req, res, next) => {
-    const passage = (req.query.q || '').trim()
-    if (!/[a-zA-Z0-9 ]+ [0-9]+/.test(passage)) {
+  app.get('/audio', (req, res, next) => {
+    const providerId = (req.query.p || '').trim()
+    if (!providers[providerId]) {
       res.status(404)
       return
     }
 
-    console.log('GET /chapter-audio?q=%s', passage)
-    const url = `https://api.esv.org/v3/passage/audio/?q=${encodeURIComponent(passage)}`
+    const reference = (req.query.q || '').trim()
+    console.log('GET /audio?p=%s&q=%s', providerId, reference)
 
-    downloadAndProcessAudio(passage)
+    getAudio(providerId, reference)
       .then(file => res.sendFile(file, { acceptRanges: false }))
       .then(null, e => next(e))
   })
 
   app.post('/record-history', (req, res) => {
-    appendHistoryRecord(defaultUsername, req.query.list, req.query.chapter)
+    appendHistoryRecord(defaultUsername, req.query.list, req.query.reference)
     res.send('')
   })
 
@@ -276,35 +169,28 @@ function startServer () {
   app.listen(3000, function () {
     console.log('Started Horner Bible audio system on port 3000')
   })
-
-  sweepOldAudio()
-    .then(null, e => console.warn('Error in sweeping:', e))
 }
 
-
 //
-// ESV API functions
+// Audio/provider API functions
 //
 
 const preloadQueue = []
 let preloadGoing = false
-function preloadAudios (passages) {
-  passages.forEach(p => preloadQueue.push(p))
-
-  sweepOldAudio()
-    .then(null, e => console.warn('Error in sweeping:', e))
+function preloadAudios (playlist) {
+  playlist.forEach(p => preloadQueue.push(p))
 
   function pumpOne () {
-    if (passages.length === 0) {
+    if (preloadQueue.length === 0) {
       preloadGoing = false
       return
     }
 
     preloadGoing = true
 
-    const next = passages.shift()
-    downloadAndProcessAudio(next)
-      .catch(e => console.warn('Error in preload of chapter:', next, e))
+    const next = preloadQueue.shift()
+    getAudio(next.provider, next.reference)
+      .catch(e => console.warn('Error in preload of audio:', next, e))
       .then(() => pumpOne())
   }
 
@@ -314,138 +200,35 @@ function preloadAudios (passages) {
 }
 
 const downloadState = {}
-function downloadAndProcessAudio (passage) {
-  const existingPromise = downloadState[passage]
+function getAudio (providerId, reference) {
+  const provider = providers[providerId]
+  const audioId = providerId + '_' + reference
+
+  const existingPromise = downloadState[audioId]
   if (existingPromise) return existingPromise
 
-  const p = downloadChapterAudio(passage)
-    .then(file => {
-      const refFile = file.replace('.mp3', '_ref.wav')
-      const videoFile = file.replace('.mp3', '.webm')
-      return skipIfExists(videoFile,
-        () => sayReference(passage, refFile)
-          .then(() => transcodeAudioToVideo([refFile, file], videoFile)))
-    })
+  const p = provider.getAudio(provider.config, reference)
 
-  downloadState[passage] = p
+  downloadState[audioId] = p
   p.then(
-    () => { delete downloadState[passage] },
-    () => { delete downloadState[passage] })
+    () => { delete downloadState[audioId] },
+    () => { delete downloadState[audioId] })
 
   return p
 }
-
-function downloadChapterAudio (chapter) {
-  const url = `https://api.esv.org/v3/passage/audio/?q=${encodeURIComponent(chapter)}`
-  const chapterId = chapter.toLowerCase().replace(/[^a-z0-9]+/g, '_')
-
-  const chapterFile = path.join(audioCacheDirectory, chapterId) + '.mp3'
-
-  return promisify(fs.mkdir)(audioCacheDirectory)
-    .catch(() => null)
-    .then(() => promisify(fs.exists)(chapterFile))
-    .then(exists => {
-      if (exists) return chapterFile
-
-
-      console.log('getting chapter from ESV:', chapter)
-
-      return new Promise((resolve, reject) => {
-        request({
-          url,
-          headers: {
-            'Authorization': 'Token ' + apiKey
-          },
-          encoding: null
-        })
-          .pipe(fs.createWriteStream(chapterFile))
-          .on('close', () => resolve())
-          .on('error', e => reject(e));
-      })
-    })
-    .then(() => chapterFile)
-}
-
-function transcodeAudioToVideo (files, outFile) {
-  const tmpFile = outFile.replace('.', '_tmp.')
-  const concatConfig = files.map((f, idx) => `[${idx}:0]`).join('')
-    + `concat=n=${files.length}:v=0:a=1[out]`
-  const cmd1 = [
-    '-y',
-    ...[].concat(...files.map(f => ['-i', f])),
-    '-filter_complex', concatConfig, '-map', '[out]',
-    tmpFile]
-
-  const cmd2 = [
-    '-y',
-    '-i', tmpFile,
-    '-f', 'lavfi', '-i', 'color=c=black:s=720x406:r=25:sar=1/1:d=13',
-    '-c:a', 'copy',
-    outFile]
-
-  return promisify(childProcess.execFile)('ffmpeg', cmd1)
-    .then(() => promisify(childProcess.execFile)('ffmpeg', cmd2))
-    .then(() => console.log('transcoded to', outFile))
-    .then(() => outFile)
-}
-
-function sayReference (chapter, outFile) {
-  console.log('saying reference:', chapter)
-  const cmd = [
-    '-s', '130',
-    '-p', '30',
-    chapter,
-    '-w', outFile]
-  return promisify(childProcess.execFile)('espeak', cmd)
-    .then(() => outFile)
-}
-
-function skipIfExists (file, fn) {
-  return promisify(fs.exists)(file)
-    .then(exists => exists ? file : fn())
-}
-
-
-function sweepOldAudio () {
-  const minKeepMs = 1 * 60 * 60 * 1000 // for sanity, keep for at least an hour
-  const sweepAgeMs = Math.max(sweepAgeDays * 24 * 60 * 60 * 1000, minKeepMs)
-  const now = Date.now()
-
-  return promisify(fs.readdir)(audioCacheDirectory)
-    .then(files => {
-      return Promise.all(files.map(f => {
-        const p = path.join(audioCacheDirectory, f)
-        return promisify(fs.stat)(p)
-          .then(stat => ({ file: p, stat }))
-      }))
-    })
-    .then(fileStats => {
-      return Promise.all(fileStats.map(f => {
-        const age = now - f.stat.mtimeMs
-        const shouldDelete = age > sweepAgeMs
-
-        if (shouldDelete) {
-          console.log('deleting', path.basename(f.file), { age, sweepAgeMs })
-          return promisify(fs.unlink)(f.file)
-        } else return null
-      }))
-    })
-}
-
 
 //
 // history functions
 //
 
-function appendHistoryRecord (username, listName, chapter) {
-  const entry = JSON.stringify([username, listName, chapter, new Date().toISOString()])
+function appendHistoryRecord (username, listName, reference) {
+  const entry = JSON.stringify([username, listName, reference, new Date().toISOString()])
   promisify(fs.appendFile)(historyFile, entry + '\n')
     .then(() => {
-      console.log('recorded chapter %s of list %s', chapter, listName)
-      updatePlaceToChapter(listName, chapter)
+      console.log('recorded reference %s of list %s', reference, listName)
     })
     .then(null, e => {
-      console.warn('failed to record %s %s:', listName, chapter, e)
+      console.warn('failed to record %s %s:', listName, reference, e)
       setTimeout(() => process.exit(1), 500)
     })
 }
@@ -461,7 +244,7 @@ function loadHistory (username) {
           return {
             username: json[0],
             listName: json[1],
-            chapter: json[2],
+            reference: json[2],
             timestamp: json[3]
           }
         })
@@ -476,97 +259,29 @@ function loadHistory (username) {
     })
 }
 
+function loadConfig () {
+  return promisify(fs.readFile)(configFile, 'utf8')
+    .then(text => {
+      return JSON.parse(text)
+    })
+    .then(null, e => {
+      console.warn('failed to load config:', e)
+      setTimeout(() => process.exit(1), 500)
+      throw e
+    })
+}
 
 //
 // state functions
 //
 
-function updatePlaceToCount (listName, count) {
-  const list = lists.find(l => l.name === listName)
-  const place = state.find(p => p.name === listName)
-  if (!list) throw new Error('Unknown list: ' + listName)
-
-  place.index = count % list.chapterCount
-  place.playSequenceClock = nextPlaySequenceClock()
-}
-
-function updatePlaceToChapter (listName, chapter) {
-  const list = lists.find(l => l.name === listName)
-  const place = state.find(p => p.name === listName)
-  if (!list) throw new Error('Unknown list: ' + listName)
-
-  const chapterIndex = list.chapterSequence.indexOf(chapter)
-  if (chapterIndex >= 0) place.index = chapterIndex
-  else throw new Error('Unknown chapter ' + chapter + ' for list ' + listName)
-  place.playSequenceClock = nextPlaySequenceClock()
-}
-
-function incrementNextPlace (listName) {
-  const list = lists.find(l => l.name === listName)
-  const place = state.find(p => p.name === listName)
-  if (!list) throw new Error('Unknown list: ' + listName)
-
-  place.index++
-  if (place.index >= list.chapterCount) place.index = 0
-  place.playSequenceClock = nextPlaySequenceClock()
-}
-
-
-function getNextChapterByPlace (listName) {
-  const list = lists.find(l => l.name === listName)
-  const place = state.find(p => p.name === listName)
-  if (!list) throw new Error('Unknown list: ' + listName)
-
-  return list.chapterSequence[(place.index + 1) % list.chapterCount]
-}
-
-function nextPlaySequenceClock () {
-  return Math.max(-1, ...state.map(p => p.playSequenceClock).filter(p => p !== null)) + 1
-}
-
-function getPlaylist () {
-  let idx = indexOfMax(state, p => p.playSequenceClock) + 1;
-
-  const chapters = []
-  for (let i = 0; i < lists.length; i++) {
-    if (idx >= lists.length) idx = 0
-
-    chapters.push({
-      list: lists[idx].name,
-      chapter: getNextChapterByPlace(lists[idx].name)
-    })
-
-    idx++
-  }
-  return chapters
-}
-
-
-//
-// helpers
-//
-
-function rangeUntil (count) {
-  const array = []
-  for (let x = 0; x < count; x++) {
-    array.push(x)
-  }
-  return array
-}
-
-function toD1 (n) {
-  return Math.round(n * 10) / 10
-}
-
-function indexOfMax (list, fn) {
-  let index = 0
-  let max = -Infinity
-  list.forEach((item, idx) => {
-    const value = fn(item) || 0
-    if (max < value) {
-      max = value
-      index = idx
+function getPlaylist (config, state) {
+  return config.lists.map(list => {
+    const provider = providers[list.provider]
+    return {
+      name: list.name,
+      provider: list.provider,
+      reference: provider.getNext(provider.config, list, state[list.name])
     }
   })
-  return index
 }
---
+++ src/esv_provider.js
@@ -0,0 +1,228 @@
+const childProcess = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+const request = require('request')
+
+const dataDirectory = path.join(__dirname, '..', 'data')
+const audioCacheDirectory = path.join(dataDirectory, 'esv_cache')
+const sweepAgeDays = process.env.SWEEP_AGE_DAYS || 2
+
+const bookChapterCounts = {
+  'Genesis': 50,
+  'Exodus': 40,
+  'Leviticus': 27,
+  'Numbers': 36,
+  'Deuteronomy': 34,
+  'Joshua': 24,
+  'Judges': 21,
+  'Ruth': 4,
+  '1 Samuel': 31,
+  '2 Samuel': 24,
+  '1 Kings': 22,
+  '2 Kings': 25,
+  '1 Chronicles': 29,
+  '2 Chronicles': 36,
+  'Ezra': 10,
+  'Nehemiah': 13,
+  'Esther': 10,
+  'Job': 42,
+  'Psalms': 150,
+  'Proverbs': 31,
+  'Ecclesiastes': 12,
+  'Song of Solomon': 8,
+  'Isaiah': 66,
+  'Jeremiah': 52,
+  'Lamentations': 5,
+  'Ezekiel': 48,
+  'Daniel': 12,
+  'Hosea': 14,
+  'Joel': 3,
+  'Amos': 9,
+  'Obadiah': 1,
+  'Jonah': 4,
+  'Micah': 7,
+  'Nahum': 3,
+  'Habakkuk': 3,
+  'Zephaniah': 3,
+  'Haggai': 2,
+  'Zechariah': 14,
+  'Malachi': 4,
+  'Matthew': 28,
+  'Mark': 16,
+  'Luke': 24,
+  'John': 21,
+  'Acts': 28,
+  'Romans': 16,
+  '1 Corinthians': 16,
+  '2 Corinthians': 13,
+  'Galatians': 6,
+  'Ephesians': 6,
+  'Philippians': 4,
+  'Colossians': 4,
+  '1 Thessalonians': 5,
+  '2 Thessalonians': 3,
+  '1 Timothy': 6,
+  '2 Timothy': 4,
+  'Titus': 3,
+  'Philemon': 1,
+  'Hebrews': 13,
+  'James': 5,
+  '1 Peter': 5,
+  '2 Peter': 3,
+  '1 John': 5,
+  '2 John': 1,
+  '3 John': 1,
+  'Jude': 1,
+  'Revelation': 22
+}
+
+function generateChaptersForList (bookNames) {
+  return [].concat(...bookNames.map(b => {
+    const bookLength = bookChapterCounts[b]
+    if (bookLength === 1) return [b]
+    else return rangeUntil(bookLength).map(i => b + ' ' + (i + 1))
+  }))
+}
+
+//
+// ESV API functions
+//
+
+function downloadAndProcessAudio (apiKey, passage) {
+  return downloadChapterAudio(apiKey, passage)
+    .then(file => {
+      const refFile = file.replace('.mp3', '_ref.wav')
+      const videoFile = file.replace('.mp3', '.webm')
+      return skipIfExists(videoFile,
+        () => sayReference(passage, refFile)
+          .then(() => transcodeAudioToVideo([refFile, file], videoFile)))
+    })
+}
+
+function downloadChapterAudio (apiKey, chapter) {
+  const url = `https://api.esv.org/v3/passage/audio/?q=${encodeURIComponent(chapter)}`
+  const chapterId = chapter.toLowerCase().replace(/[^a-z0-9]+/g, '_')
+
+  const chapterFile = path.join(audioCacheDirectory, chapterId) + '.mp3'
+
+  return promisify(fs.mkdir)(audioCacheDirectory)
+    .catch(() => null)
+    .then(() => promisify(fs.exists)(chapterFile))
+    .then(exists => {
+      if (exists) return chapterFile
+
+      console.log('getting chapter from ESV:', chapter)
+
+      return new Promise((resolve, reject) => {
+        request({
+          url,
+          headers: {
+            'Authorization': 'Token ' + apiKey
+          },
+          encoding: null
+        })
+          .pipe(fs.createWriteStream(chapterFile))
+          .on('error', e => reject(e))
+          .on('close', () => resolve())
+      })
+    })
+    .then(() => chapterFile)
+}
+
+function transcodeAudioToVideo (files, outFile) {
+  const tmpFile = outFile.replace('.', '_tmp.')
+  const concatConfig = files.map((f, idx) => `[${idx}:0]`).join('') +
+    `concat=n=${files.length}:v=0:a=1[out]`
+  const cmd1 = [
+    '-y',
+    ...[].concat(...files.map(f => ['-i', f])),
+    '-filter_complex', concatConfig, '-map', '[out]',
+    tmpFile]
+
+  const cmd2 = [
+    '-y',
+    '-i', tmpFile,
+    '-f', 'lavfi', '-i', 'color=c=black:s=720x406:r=25:sar=1/1:d=13',
+    '-c:a', 'copy',
+    outFile]
+
+  return promisify(childProcess.execFile)('ffmpeg', cmd1)
+    .then(() => promisify(childProcess.execFile)('ffmpeg', cmd2))
+    .then(() => console.log('transcoded to', outFile))
+    .then(() => outFile)
+}
+
+function sayReference (chapter, outFile) {
+  console.log('saying reference:', chapter)
+  const cmd = [
+    '-s', '130',
+    '-p', '30',
+    chapter,
+    '-w', outFile]
+  return promisify(childProcess.execFile)('espeak', cmd)
+    .then(() => outFile)
+}
+
+function skipIfExists (file, fn) {
+  return promisify(fs.exists)(file)
+    .then(exists => exists ? file : fn())
+}
+
+function sweepOldAudio () {
+  const minKeepMs = 1 * 60 * 60 * 1000 // for sanity, keep for at least an hour
+  const sweepAgeMs = Math.max(sweepAgeDays * 24 * 60 * 60 * 1000, minKeepMs)
+  const now = Date.now()
+
+  return promisify(fs.readdir)(audioCacheDirectory)
+    .then(files => {
+      return Promise.all(files.map(f => {
+        const p = path.join(audioCacheDirectory, f)
+        return promisify(fs.stat)(p)
+          .then(stat => ({ file: p, stat }))
+      }))
+    })
+    .then(fileStats => {
+      return Promise.all(fileStats.map(f => {
+        const age = now - f.stat.mtimeMs
+        const shouldDelete = age > sweepAgeMs
+
+        if (shouldDelete) {
+          console.log('deleting', path.basename(f.file))
+          return promisify(fs.unlink)(f.file)
+        } else return null
+      }))
+    })
+}
+
+//
+// helpers
+//
+
+function rangeUntil (count) {
+  const array = []
+  for (let x = 0; x < count; x++) {
+    array.push(x)
+  }
+  return array
+}
+
+module.exports = {
+  name: 'esv',
+
+  getNext (config, listConfig, reference) {
+    const chapters = generateChaptersForList(listConfig.books)
+
+    let chapterIndex = chapters.indexOf(reference)
+    if (chapterIndex < 0) chapterIndex = -1
+
+    return chapters[(chapterIndex + 1) % chapters.length]
+  },
+
+  getAudio (config, reference) {
+    sweepOldAudio()
+      .then(null, e => console.warn('Error in sweeping:', e))
+    return downloadAndProcessAudio(config.api_key, reference)
+  }
+}
